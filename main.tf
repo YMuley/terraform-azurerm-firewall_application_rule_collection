9ef9f5bca1380ca157f66ff822293926ff6b7b97
sunanda sharma <sunandasharma@ddiworld.com>
2024-02-13 13:43:02 +0530

    testing to be done

---
+++ main.tf
@@ -0,0 +1,28 @@
+#--------- firewall application rule collection ------------
+resource "azurerm_firewall_application_rule_collection" "azure_firewall_application_rule_collection" {
+  for_each            = local.azure_firewall_application_rule_collection
+  name                = each.value.name
+  resource_group_name = each.value.resource_group_name
+  azure_firewall_name = each.value.azure_firewall_name
+  priority            = each.value.priority
+  action              = each.value.action
+
+  dynamic "rule" {
+    for_each = each.value.rule_list
+    content {
+      name             = rule.value.name
+      source_addresses = rule.value.source_addresses
+      target_fqdns     = rule.value.target_fqdns
+
+      dynamic "protocol" {
+        for_each = rule.value.protocol_list
+        content {
+          port = protocol.value.port
+          type = protocol.value.type
+
+        }
+      }
+    }
+  }
+}
+
---
+++ output.tf
@@ -0,0 +1,4 @@
+output "azure_firewall__application_rule_collection_output" {
+  value       = azurerm_firewall_application_rule_collection.azure_firewall_application_rule_collection
+  description = "Outputs of azure firewall application rule collection objects"
+}
